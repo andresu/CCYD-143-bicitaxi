b71daa86c73743e85d5b95d50fb576970b8a0ea3
Andres Urrea <andres.urreao@gmail.com>
2018-10-31 18:52:53 -0500

    Install gem sentr-raven

--- backend/Bicitaxi-app/config/application.rb
+++ backend/Bicitaxi-app/config/application.rb
@@ -15,5 +15,13 @@ module BicitaxiApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    Raven.configure do |config|
+      config.dsn = 'https://1cbe31e620d4417292c4e9ade27325bd:fb80d60242c849ab94adb51a37637e8a@sentry.io/1313462'
+    end
+
+    Raven.configure do |config|
+      config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    end
+    
   end
 end
