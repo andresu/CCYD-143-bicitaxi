7a2d735cdb90e3b48eb5349311e93fcbac0b5a8d
tovarvictro986 <tovarvictro986@gmail.com>
2018-11-01 20:41:16 -0500

    configuracion SENTRY

--- backend/Bicitaxi-app/config/application.rb
+++ backend/Bicitaxi-app/config/application.rb
@@ -16,12 +16,13 @@ module BicitaxiApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     Raven.configure do |config|
-      config.dsn = 'https://1cbe31e620d4417292c4e9ade27325bd:fb80d60242c849ab94adb51a37637e8a@sentry.io/1313462'
+      config.dsn = 'https://485a1c6c40a74c4aa53cfec345f49865:2e3d0ccad83547bba333e6f319e3dd5b@sentry.io/1314254'
     end
 
+    # in an initializer, like sentry.rb
     Raven.configure do |config|
       config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
     end
-    
+        
   end
 end
